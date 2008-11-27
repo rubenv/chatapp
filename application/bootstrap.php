a52dba070ffc99d69537c4d47802600c26378ceb
Ruben Vermeersch <ruben@savanne.be>
2008-11-27 13:42:59 +0100

    Initial import.

---
+++ application/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+require dirname(__FILE__).'/global.php';
+
+Zend_Controller_Front::run(dirname(__FILE__).'/controllers');
+?>
---
+++ application/controllers/IndexController.php
@@ -0,0 +1,43 @@
+<?php
+class IndexController extends Zend_Controller_Action
+{
+    public function indexAction()
+    {
+        $form = $this->getForm();
+        $req = $this->getRequest();
+        if ($req->getPost() && $form->isValid($req->getPost())) {
+            // TODO: Insert message into database
+            $message = new Message();
+            $message->name = $this->getRequest()->getPost('name');
+            $message->message = $this->getRequest()->getPost('message');
+            $message->posted = new Doctrine_Expression('NOW()');
+            $message->save();
+        }
+        $this->view->form = $form;
+
+        $messages = Doctrine_Query::create()
+                    ->from('Message m')
+                    ->orderBy('m.posted DESC')
+                    ->execute();
+        $this->view->messages = $messages;
+
+        // TODO: Retrieve all messages.
+    }
+
+    private function getForm()
+    {
+        $form = new Zend_Form();
+        $form->addElement('text', 'name', array(
+                    'label' => 'Your name',
+                    'required' => true
+                    ));
+        $form->addElement('textarea', 'message', array(
+                    'label' => 'Message',
+                    'required' => true,
+                    'rows' => 4
+                    ));
+        $form->addElement('submit', 'send');
+        return $form;
+    }
+}
+?>
